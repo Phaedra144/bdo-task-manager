62fba2e2d6f9760fe22ce950d4494e191c858d95
Phaedra144 <bedi.szilvia@gmail.com>
2024-11-04 10:33:59 +0100

    Make User and Task db tables, create unidirectional one-to-many relation between them

--- taskmanager/src/main/java/com/bdo/taskmanager/entity/Task.java
+++ taskmanager/src/main/java/com/bdo/taskmanager/entity/Task.java
@@ -1,6 +1,18 @@
 package com.bdo.taskmanager.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
   private String title;
   private String description;
 
--- taskmanager/src/main/java/com/bdo/taskmanager/entity/User.java
+++ taskmanager/src/main/java/com/bdo/taskmanager/entity/User.java
@@ -1,45 +1,82 @@
 package com.bdo.taskmanager.entity;
 
-import jakarta.persistence.Embedded;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
   private String fullName;
   private String password;
   private String email;
   @Embedded
   private Address address;
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private List<Task> tasks;
 
   public User() {
   }
-  public User(String fullName, String password, String email, Address address) {
+
+  public User(String fullName, String password, String email, Address address, List<Task> tasks) {
     this.fullName = fullName;
     this.password = password;
     this.email = email;
     this.address = address;
+    this.tasks = tasks;
   }
+
   public String getFullName() {
     return fullName;
   }
+
   public void setFullName(String fullName) {
     this.fullName = fullName;
   }
+
   public String getPassword() {
     return password;
   }
+
   public void setPassword(String password) {
     this.password = password;
   }
+
   public String getEmail() {
     return email;
   }
+
   public void setEmail(String email) {
     this.email = email;
   }
+
   public Address getAddress() {
     return address;
   }
+
   public void setAddress(Address address) {
     this.address = address;
+  }
+
+  public List<Task> getTasks() {
+    return tasks;
+  }
+
+  public void setTasks(List<Task> tasks) {
+    this.tasks = tasks;
   }
 
 }
