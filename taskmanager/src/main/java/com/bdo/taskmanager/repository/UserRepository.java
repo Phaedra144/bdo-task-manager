30c71252df5d93e48d5d763ed49cd6a83ef21bda
Phaedra144 <bedi.szilvia@gmail.com>
2024-11-04 20:35:33 +0100

    Create jpa queries for users that are not deleted

--- taskmanager/src/main/java/com/bdo/taskmanager/repository/UserRepository.java
+++ taskmanager/src/main/java/com/bdo/taskmanager/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.bdo.taskmanager.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,12 @@ import com.bdo.taskmanager.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-  @Query("SELECT u FROM User u WHERE u.id = :id")
-  Optional<User> findByIdEvenIfDeleted(@Param("id") Integer id);
+  @Query("SELECT u FROM User u WHERE u.id = :id AND u.deleted = false")
+  Optional<User> findByIdNonDeleted(@Param("id") Integer id);
+
+  @Query("SELECT u FROM User u LEFT JOIN FETCH u.tasks t WHERE u.id = :id AND u.deleted = false AND (t.deleted = false OR t.deleted IS NULL)")
+  Optional<User> findByIdWithNonDeletedTasks(@Param("id") Integer id);
+
+  @Query("select u from User u left join fetch u.tasks t where u.deleted = false and (t.deleted = false or t.deleted is null)")
+  List<User> findAllNonDeletedWithNonDeletedTasks();
 }
