0ed81a197cdc65e47d8e911f1150f912f653886c
Phaedra144 <bedi.szilvia@gmail.com>
2024-11-05 15:05:44 +0100

    Add cors configuration

--- taskmanager/src/main/java/com/bdo/taskmanager/TaskmanagerApplication.java
+++ taskmanager/src/main/java/com/bdo/taskmanager/TaskmanagerApplication.java
@@ -2,12 +2,30 @@ package com.bdo.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
+						.maxAge(-1) // add maxAge
+						.allowCredentials(false);
+			}
+		};
 	}
 
 }
